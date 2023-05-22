7ba5bd6496500333258d9c98818988fad0b6899d
Luis Ricardo <luiscastrodev@gmail.com>
2023-05-22 08:11:58 +0100

    controle fuxo

---
+++ src/main/kotlin/ControleFuxo/KontroleFuxo.kt
@@ -0,0 +1,30 @@
+package ControleFuxo
+
+fun main() {
+
+    for (i in 1..10 step 2){
+        println(i)
+    }
+
+    val s = "kotlin"
+
+    for (char in s){
+        println(char)
+    }
+
+    for (j in 20  downTo 0 step 2){
+        println(j)
+    }
+
+    var index = 0
+    while (index <= 300){
+        println(index)
+        index ++
+    }
+
+    do {
+        println(index)
+        index ++
+    }while (index <= 500)
+}
+
